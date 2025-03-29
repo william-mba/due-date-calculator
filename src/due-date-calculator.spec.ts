098c0b71a3311c5d1120f75f0a70bd7ca7bf7217
William Mba <wmba@ecologiciel.com>
2025-03-29 13:59:21 -0400

    fix: text

--- src/due-date-calculator.spec.ts
+++ src/due-date-calculator.spec.ts
@@ -5,8 +5,7 @@ describe('CalculateDueDate method', () => {
   it('shoud calculate the due date/time', () => {
     const reportTime = new Date(2025, 2, 25, 14, 12, 0);
     const dueDate = calculateDuteDate(reportTime, 16);
-    const expectedTime = new Date(2025, 2, 27, 2, 12, 0);
-    expectedTime.setHours(16);
+    const expectedTime = new Date(2025, 2, 27, 14, 12, 0);
 
     const intl = Intl.DateTimeFormat('en-US', {
       timeZone: 'America/New_York',
